f26b239c2bd4a31fb6b0f280b26ab90e99cf7247
Nigel Fong <nigel.fong@mail.utoronto.ca>
2017-04-14 05:56:35 -0400

    Add examples to Tuple

--- examples/ending_locations/Binop.py
+++ examples/ending_locations/Binop.py
@@ -6,11 +6,11 @@
 ( 100 * 42  )
 101   *  43
 ( (102) * (44)  )
-1 + (2 + 3   # some comment here (4 + 5)
+1 + (2 + 3          # some comment here (4 + 5)
     )
-(
+(                   # line is ignored by node fromelino location setting.
     1 + (2 + 3)
-)
+)                   # line is ignored by node end_lineno location setting.
 ( (111) * (
     22
     ))
